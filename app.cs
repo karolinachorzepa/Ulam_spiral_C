3c1eee7d547c09949d0d724bd3b1f12d103f64e3
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2021-03-26 19:49:45 +0100

    loopsc#

---
+++ app.cs
@@ -0,0 +1,11 @@
+class App
+{
+    static void Main()
+    {
+    //default value FALSE
+    // marc NON prime numer as TRUE
+    bool[] numbers =new bool[100];
+        for (int i =2; i<100; i++
+            console.WriteLine(i+" "+numbers[i]));
+    }
+}
